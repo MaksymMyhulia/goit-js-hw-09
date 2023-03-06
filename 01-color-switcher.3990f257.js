6c26fe872f30c22805ee06d1fff74203887c0382
MaksymMyhulia <113797631+MaksymMyhulia@users.noreply.github.com>
2023-03-06 08:14:48 +0000

    Deploying to gh-pages from @ MaksymMyhulia/goit-js-hw-09@53db04c89d10f8808dcc6cf75828c7ffe2dd6cf0 🚀

--- 01-color-switcher.3990f257.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let n=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,t.removeAttribute("disabled"),clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.3990f257.js.map
