53db04c89d10f8808dcc6cf75828c7ffe2dd6cf0
MaksymMyhulia <113797631+MaksymMyhulia@users.noreply.github.com>
2023-03-06 10:13:51 +0200

    fixed_first_task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,13 +2,14 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
-
+let idInterval = null;
+const DELAY = 1000;
 const startButton = document.querySelector('button[data-start]');
 const stopButton = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
-let idInterval = null;
-const DELAY = 1000;
 
+
+stopButton.setAttribute('disabled', true);
 startButton.addEventListener('click', onStartButtonClick);
 stopButton.addEventListener('click', onStopButtonClick);
 
